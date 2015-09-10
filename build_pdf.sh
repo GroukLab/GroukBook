5b9f04b0cb54d3b49f57c1e538c8113295fd8318
jolestar <jolestar@gmail.com>
2015-09-10 21:41:33 +0800

    update build pdf

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,2 +1,6 @@
-gitbook pdf . _book/groukbook_zh-CN.pdf
+FILENAME="_book/groukbook_zh-CN.pdf"
+gitbook pdf . $FILENAME
+eval $(stat -s $FILENAME)
+echo '{"filename":"groukbook-zh-CN.pdf","fileSize":'$st_size',"contentType":"application\/pdf","url":"https:\/\/guide.grouk.com\/groukbook_zh-CN.pdf"}' > groukbook_attachment.json
 aws s3 cp _book/groukbook_zh-CN.pdf s3://grouk-public/groukbook/zh-CN/
+aws s3 cp groukbook_attachment.json s3://grouk-public/groukbook/zh-CN/
